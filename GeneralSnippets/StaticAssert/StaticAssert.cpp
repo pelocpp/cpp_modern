dddaa3d59d8f932a66d3298c109ae22ee6ed0d91
Peter Loos <peter.loos@gmx.de>
2020-01-01 17:46:36 +0100

    added static_assert

--- GeneralSnippets/StaticAssert/StaticAssert.cpp
+++ GeneralSnippets/StaticAssert/StaticAssert.cpp
@@ -2,9 +2,18 @@
 
 // =====================================================================================
 // https://www.geeksforgeeks.org/understanding-static_assert-c-11/
+// und
+// https://www.grimm-jaud.de/index.php/blog/statisch-geprueft
 // =====================================================================================
 
 #include <iostream> 
+
+template < typename T, int Height, int Width >
+struct Grid {
+    static_assert(Height >= 0, "Height must be positive.");
+    static_assert(Width >= 0, "Width must be positive.");
+    static_assert(Height + Width > 0, "Height and Width must be greater than 0.");
+};
 
 template <class T, int Size>
 class MyVector {
@@ -18,7 +27,15 @@ class MyVector {
 
 int main_static_assert()
 {
-    MyVector<int, 4> four;     // This will work 
-    // MyVector<short, 2> two; // This will fail 
+    static_assert(sizeof(void*) == 4, "Size of Pointer is 32-bits!");
+    // static_assert(sizeof(void*) == 8, "Size of Pointer is 64-bits!");
+
+    Grid<int, 10, 5> intGrid;
+    Grid<std::string, 3, 4> strGrid;
+    // Grid<double, 0, 0> doubleGrid;   // doesn't compile
+    Grid<long long, 1, 0> longGrid;
+
+    MyVector<int, 4> four;     // compiles
+    // MyVector<short, 2> two; // doesn't compile
     return 0;
 }
