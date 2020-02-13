a2072061519f50e5a3a89f7313df5c91280b67f6
Peter Loos <peter.loos@gmx.de>
2020-02-13 15:07:49 +0100

    added more 'fold' expressions

--- GeneralSnippets/VariadicTemplates/VariadicTemplate.cpp
+++ GeneralSnippets/VariadicTemplates/VariadicTemplate.cpp
@@ -105,32 +105,110 @@ namespace VariadicTemplates {
 
     template<typename ...Args>
     void printer(Args... args) {
+        // binary left fold (init == ostream)
         (std::cout << ... << args) << std::endl;
-    }
-
-    template <typename... Args>
-    void printerWithSeperator(Args... args)
-    {
-        std::string sep = " ";
-        ((std::cout << args << sep), ...) << std::endl;
     }
 
     // demonstrating fold expressions
     void test_04() {
-
         printer(1, 2, 3, "ABC", "DEF", "GHI");
-        printerWithSeperator(1, 2, 3, "ABC", "DEF", "GHI");
     }
 
     template<typename ...Args>
-    auto anotherAdder(Args ...args)
-    {
-        return (args + ... + 0);
+    auto anotherAdder(Args ...args) {
+        return (args + ... + 0);  // binary right fold (init == 0)
     }
 
     void test_05() {
         int sum = anotherAdder<int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         std::cout << "Sum from 1 up to 10: " << sum << std::endl;
+    }
+
+    // demonstrating all four versions of folding expressions
+
+    template<typename ...Args>
+    auto anotherSubtracterBRF(Args ...args) {
+        return (args - ... - 0);  // binary right fold (init == 0)
+    }
+
+    void test_06a() {
+        // binary right fold: 1 - (2 - (3 - (4 - ( 5 - 0)))) = 3
+        int result = anotherSubtracterBRF<int>(1, 2, 3, 4, 5);
+        std::cout << "BRF: 1 - (2 - (3 - (4 - ( 5 - 0)))): " << result << std::endl;
+    }
+
+    // -----------------------------------------------------------------------
+
+    template<typename ...Args>
+    auto anotherSubtracterBLF(Args ...args) {
+        return (0 - ... - args);  // binary left fold (init == 0)
+    }
+
+    void test_06b() {
+        // binary left fold: ((((0 - 1) - 2) - 3) - 4) - 5 =  -15
+        int result = anotherSubtracterBLF<int>(1, 2, 3, 4, 5);
+        std::cout << "BLF: ((((0 - 1) - 2) - 3) - 4) - 5: " << result << std::endl;
+    }
+
+    // -----------------------------------------------------------------------
+
+    template<typename ...Args>
+    auto anotherSubtracterURF(Args ...args) {
+        return (args - ...);  // unary right fold
+    }
+
+    void test_06c() {
+        // unary right fold: 1 - (2 - (3 - (4 - 5))) = 3
+        int result = anotherSubtracterURF<int>(1, 2, 3, 4, 5);
+        std::cout << "URF: 1 - (2 - (3 - (4 - 5))): " << result << std::endl;
+    }
+
+    // ----------------------------------------------------------------------
+
+    template<typename ...Args>
+    auto anotherSubtracterULF(Args ...args) {
+        return (... - args);  // unary left fold
+    }
+
+    void test_06d() {
+        // unary left fold: ((((1 - 2) - 3) - 4) - 5 = -13
+        int result = anotherSubtracterULF<int>(1, 2, 3, 4, 5);
+        std::cout << "URF: ((((1 - 2) - 3) - 4) - 5: " << result << std::endl;
+    }
+
+    // -----------------------------------------------------------------------
+
+    // Folding over a comma (',' operator)
+
+    template <typename... Args>
+    void printerWithSeperator(Args... args) {
+        std::string sep = " ";
+        ((std::cout << args << sep), ...) << std::endl;
+    }
+
+    // demonstrating fold expressions
+    void test_07() {
+        printerWithSeperator(1, 2, 3, "ABC", "DEF", "GHI");
+    }
+
+    // -----------------------------------------------------------------------
+
+    // Folding over a comma - pack arguments can be handled by a separate function
+
+    template <typename T>
+    std::ostream& handleArg(T arg) {
+        std::cout << arg << "-";
+        return std::cout;
+    }
+
+    template <typename... Args>
+    void printerWithSeperator02(Args... args) {
+        (handleArg(args), ...) << std::endl;
+    }
+
+    // demonstrating fold expressions
+    void test_08() {
+        printerWithSeperator02(1, 2, 3, "ABC", "DEF", "GHI");
     }
 }
 
@@ -142,6 +220,12 @@ int main_variadic_templates()
     test_03();
     test_04();
     test_05();
+    test_06a();
+    test_06b();
+    test_06c();
+    test_06d();
+    test_07();
+    test_08();
     return 0;
 }
 
