1ee0ed623d0214011b18ba34037a6d6fece03fe4
Peter Loos <peter.loos@gmx.de>
2020-02-26 22:06:41 +0100

    added one more 'RAII' snippet

---
+++ GeneralSnippets/RAII/RAII02.cpp
@@ -0,0 +1,56 @@
+// =====================================================================================
+// RAII
+// =====================================================================================
+
+#include <string>
+#include <mutex>
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+
+// See also:  
+// https://stackoverflow.com/questions/4802494/do-i-need-to-close-a-stdfstream/4802506
+
+namespace RAIIDemo02 {
+
+    void write_to_file(const std::string& message)
+    {
+        // mutex to protect file access (shared across threads)
+        static std::mutex mtex;
+
+        // lock mutex before accessing file
+        std::lock_guard<std::mutex> lock(mtex);
+
+        // try to open file
+        std::ofstream file("example.txt");
+        if (!file.is_open())
+            throw std::runtime_error("unable to open file");
+
+        // write message to file
+        file << message << std::endl;
+
+        // file will be closed 1st when leaving scope (regardless of exception)
+        // mutex will be unlocked 2nd (from lock destructor) when leaving
+        // scope (regardless of exception)
+
+        // fstream (rsp. ofstream) is a proper RAII object,
+        // it does close automatically at the end of the scope,
+        // and there is absolutely no need whatsoever to call close manually
+        // when closing at the end of the scope is sufficient.
+    }
+
+    void test_01 () {
+        write_to_file("Nachrichtentext");
+    }
+}
+
+int main_raii_02()
+{
+    using namespace RAIIDemo02;
+    test_01();
+    return 0;
+}
+
+// =====================================================================================
+// End-of-File
+// =====================================================================================
