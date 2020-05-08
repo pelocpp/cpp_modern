dcf0e6ea9c530f064df12c202d99b3c678d6cd41
Peter Loos <peter.loos@gmx.de>
2020-05-08 11:59:22 +0200

    added file system snippet

--- GeneralSnippets/FileSystem/FileSystem.cpp
+++ GeneralSnippets/FileSystem/FileSystem.cpp
@@ -3,23 +3,146 @@
 // =====================================================================================
 
 #include <iostream>
+#include <string>
+#include <filesystem>
 
 namespace FileSystemDemo {
 
-    void test_01() {
+    void test_01(std::string path) {
+        // examine path object
+        std::cout << "Examine " << path << ':' << '\n';
+        std::filesystem::path current;
 
+        if (path.empty()) {
+            current = std::filesystem::current_path();
+        }
+        else {
+            current = { path };
+        }
+
+        if (std::filesystem::exists(current)) {
+            std::cout
+                << "exists() = " << std::filesystem::exists(current) << '\n'
+                << "root_name() = " << current.root_name() << '\n'
+                << "root_path() = " << current.root_path() << '\n'
+                << "relative_path() = " << current.relative_path() << '\n'
+                << "parent_path() = " << current.parent_path() << '\n'
+                << "filename() = " << current.filename() << '\n'
+                << "stem() = " << current.stem() << '\n'
+                << "extension() = " << current.extension() << '\n';
+        }
+        std::cout << std::endl;
     }
 
     void test_02() {
+        // iterating over path object
 
+        // note: use of raw character notation
+        std::filesystem::path p(R"(C:\Development\GitRepositoryCPlusPlus\Readme_Ssh.txt)");
+
+        // range-based loop
+        int i = 0;
+        for (const auto& part : p)
+            std::cout << "part: " << i++ << " = " << part << '\n';
+        std::cout << std::endl;
+
+        // or iterator based loop
+        i = 0;
+        std::filesystem::path::iterator begin = p.begin();
+        std::filesystem::path::iterator end = p.end();
+        while (begin != end) {
+            std::cout << "part: " << i++ << " = " << *begin << '\n';
+            begin++;
+        }
+        std::cout << std::endl;
+    }
+
+    std::uintmax_t computeFileSize(const std::filesystem::path& file) {
+
+        std::filesystem::path p{ file };
+        if (std::filesystem::exists(p) && std::filesystem::is_regular_file(p)) {
+            std::error_code err = std::error_code{};
+            auto filesize = std::filesystem::file_size(p, err);
+            if (filesize != static_cast<uintmax_t>(-1)) {
+                return filesize;
+            }
+        }
+
+        return static_cast<uintmax_t>(-1);
+    }
+
+    void test_03(std::string path) {
+        std::uintmax_t size = computeFileSize(path);
+        std::cout << "Size of File: " << size << '\n';
+    }
+
+    // traversing a directory recursively
+    void displayFileInfo(const std::filesystem::directory_entry& entry, std::string& lead, const std::filesystem::path& filename)
+    {
+        std::cout << lead << filename << ", " << computeFileSize(entry) << "\n";
+    }
+
+    void displayDirectoryTreeImp(const std::filesystem::path& p, int level)
+    {
+        if (std::filesystem::exists(p) && std::filesystem::is_directory(p)) {
+            std::string lead = (level ==0) ? std::string("") : std::string(level * 4, ' ');
+
+            for (const auto& entry : std::filesystem::directory_iterator(p)) {
+                const std::filesystem::path filename = entry.path().filename();
+                if (std::filesystem::is_directory(entry.status())) {
+                    std::cout << lead << "[+] " << filename << "\n";
+                    displayDirectoryTreeImp(entry, level + 1);
+                    std::cout << "\n";
+                }
+                else if (std::filesystem::is_regular_file(entry.status())) {
+                    displayFileInfo(entry, lead, filename);
+                }
+                else {
+                    std::cout << lead << "[?]" << filename << "\n";
+                }
+            }
+        }
+    }
+
+    void displayDirectoryTree(const std::filesystem::path& p)
+    {
+        displayDirectoryTreeImp(p, 0);
+    }
+
+    void test_04(std::string path) {
+        // examine path object
+        std::cout << "Examine " << path << ':' << '\n';
+        std::filesystem::path current;
+
+        if (path.empty()) {
+            current = std::filesystem::current_path();
+        }
+        else {
+            current = { path };
+        }
+
+        displayDirectoryTree(current);
+
+        std::cout << std::endl;
     }
 }
 
 int main_filesystem()
 {
     using namespace FileSystemDemo;
-    test_01();
-    test_02();
+
+    test_01("");
+    test_01("C:\\Development\\GitRepositoryCPlusPlus\\Readme_Ssh.txt");
+    test_01("C:\\");
+    test_01("C:\\Usersss");
+
+    // test_02();
+
+    // test_03("C:\\Development\\GitRepositoryCPlusPlus\\Readme_Ssh.txt");
+
+    // note: use of raw character notation
+    // test_04(R"(C:\Development\GitRepositoryCPlusPlus\Cpp_DesignPatterns\CompositePattern)");
+    
     return 0;
 }
 
--- GeneralSnippets/FileSystem/FileSystem.md
+++ GeneralSnippets/FileSystem/FileSystem.md
@@ -1,2 +1,155 @@
 # C++17 File System
 
+## Allgemeines
+
+Die Bibliothek für das C++17 File System befindet sich in der Header-Datei `<filesystem>`.
+Es wird der Namensraum `std::filesystem` verwendet.
+
+Im wesentlichen werden in dieser Bibliothek die folgenden Kernaskekte eines Dateihandlings abgedeckt:
+
+  * Die `path` Klasse
+  * Die `directory_entry` Klasse
+  * Iteratoren für Verzeichnisse
+  * Support-Funktionen:
+    *  Informationsfunktionen zu `path`-Objekten
+    *  Dateimanipulationen: Kopieren, Verschieben, Erstellen, Symbolische Links
+    *  Zeitstempel
+    *  Berechtigungen
+    *  Speicherplatz / Dateigröße
+    *  ...
+
+In den Beispiele dieses Code-Snippets werden folgende Teilaspakte des C++17 Filesystems veranschaulicht:
+
+  * `path`-Objekt analysieren
+  * Iterieren über ein `path`-Objekt
+  * Dateigröße berechnen
+  * rekursives Traversieren eines Verzeichnisses
+
+---
+
+## Hinweis:
+
+Viele der Funktionen, die auf einem `path`-Objekt arbeiten, gibt es in zwei Versionen:
+
+  * Eine Version, die eine `filesystem_error`-Ausnahme im Fehlerfall auslöst.
+  * Eine zweite Version, die ein `error_code`-Objekt verwaltet (systemspezifisch).
+
+---
+
+## Beispiele:
+
+In vielen Beispielen im Web wird für den Namensraum `std::filesystem` ein Alias-Bezeichner `fs` einfgeführt:
+
+```cpp
+namespace fs = std::filesystem;
+```
+
+
+###### Analyse `path`-Objekt:
+
+```cpp
+Examine :
+exists() = 1
+root_name() = "C:"
+root_path() = "C:\\"
+relative_path() = "Development\\GitRepositoryCPlusPlus\\Cpp_GeneralSnippets\\GeneralSnippets"
+parent_path() = "C:\\Development\\GitRepositoryCPlusPlus\\Cpp_GeneralSnippets"
+filename() = "GeneralSnippets"
+stem() = "GeneralSnippets"
+extension() = ""
+
+Examine C:\Development\GitRepositoryCPlusPlus\Readme_Ssh.txt:
+exists() = 1
+root_name() = "C:"
+root_path() = "C:\\"
+relative_path() = "Development\\GitRepositoryCPlusPlus\\Readme_Ssh.txt"
+parent_path() = "C:\\Development\\GitRepositoryCPlusPlus"
+filename() = "Readme_Ssh.txt"
+stem() = "Readme_Ssh"
+extension() = ".txt"
+
+Examine C:\:
+exists() = 1
+root_name() = "C:"
+root_path() = "C:\\"
+relative_path() = ""
+parent_path() = "C:\\"
+filename() = ""
+stem() = ""
+extension() = ""
+
+Examine C:\Usersss:
+```
+
+###### `path`-Objekt Iteration:
+
+```cpp
+part: 0 = "C:"
+part: 1 = "\\"
+part: 2 = "Development"
+part: 3 = "GitRepositoryCPlusPlus"
+part: 4 = "Readme_Ssh.txt"
+
+part: 0 = "C:"
+part: 1 = "\\"
+part: 2 = "Development"
+part: 3 = "GitRepositoryCPlusPlus"
+part: 4 = "Readme_Ssh.txt"
+```
+
+###### Dateigröße berechnen
+
+```cpp
+Size of File: 326
+```
+
+###### Rekursives Traversieren eines Verzeichnisses
+
+```cpp
+Examine C:\Development\GitRepositoryCPlusPlus\Cpp_DesignPatterns\CompositePattern:
+"CompositePattern.vcxproj", 8046
+"CompositePattern.vcxproj.filters", 2603
+"CompositePattern.vcxproj.user", 168
+"ConceptualExample.cpp", 5989
+[+] "Debug"
+    "CompositePattern.log", 145
+    [+] "CompositePattern.tlog"
+        "CL.command.1.tlog", 8986
+        "CL.read.1.tlog", 141586
+        "CL.write.1.tlog", 15782
+        "CompositePattern.lastbuildstate", 188
+        "link.command.1.tlog", 2928
+        "link.read.1.tlog", 5790
+        "link.write.1.tlog", 1894
+
+    "ConceptualExample.obj", 337447
+    "Directory.obj", 253940
+    "Example.obj", 171620
+    "File.obj", 211037
+    "FileComponent.obj", 869
+    "FileSystemExample.obj", 171658
+    "FileSystemExample01.obj", 171826
+    "FileSystemExample02.obj", 987421
+    "FileSystemExample03.obj", 752601
+    "Program.obj", 37515
+    "vc142.idb", 322560
+    "vc142.pdb", 1232896
+
+"Directory.cpp", 1032
+"Directory.h", 737
+"File.cpp", 816
+"File.h", 676
+"FileComponent.h", 453
+"FileSystemExample01.cpp", 1152
+"FileSystemExample02.cpp", 3489
+"FileSystemExample03.cpp", 2012
+"Program.cpp", 987
+[+] "Resources"
+    "CompositePattern.png", 24524
+    "CompositePattern_02.jpg", 43563
+    "CompositePattern_02.png", 19732
+    "dp_composite_pattern.png", 17626
+    "Readme.md", 3514
+```
+
+---
