d8619db8fb4a0293f9d626d5c9dd1cd9071959d4
Peter Loos <peter.loos@gmx.de>
2020-11-17 17:49:44 +0100

    added visitor to 'std::variant' example

--- GeneralSnippets/Variant/StdVariant.md
+++ GeneralSnippets/Variant/StdVariant.md
@@ -1,12 +1,24 @@
-# std::variant
+# `std::variant`
 
 [Quellcode](Variant.cpp)
 
 ---
 
-*Allgemeines*:
+Neben der Klasse `std::variant` wird im Beispiel 
+auch auf das *Visitor*-Konzept eingegangen.
 
-Siehe das Beispiel im korrespondieren Quellcode.
+Klasse `std::variant`:
+
+  * `index()`
+  * `std::get<>()`
+  * `emplace<>()`
+  * `std::get_if<>()`
+  * `std::bad_variant_access`
+
+*Visitor*-Konzept:
+
+  * `std::visit()`
+  * `std::common_type`
 
 ---
 
--- GeneralSnippets/Variant/Variant.cpp
+++ GeneralSnippets/Variant/Variant.cpp
@@ -22,25 +22,25 @@ namespace VariantDemo {
 
         var = std::string("Hello"); // we're now a string
 
-        std::cout 
+        std::cout
             << var.index()
-            << ", value " 
+            << ", value "
             << std::get<std::string>(var)
             << std::endl;
 
         var = 10; // we're now an int
 
-        std::cout 
+        std::cout
             << var.index()
-            << ", value " 
+            << ", value "
             << std::get<int>(var)
             << std::endl;
 
         var.emplace<2>(std::string("Hello")); // we're now a string again
 
-        std::cout 
+        std::cout
             << var.index()
-            << ", value " 
+            << ", value "
             << std::get<std::string>(var)
             << std::endl;
 
@@ -58,9 +58,9 @@ namespace VariantDemo {
         if (auto pFloat = std::get_if<float>(&var); pFloat)
             *pFloat *= 2.0f;
 
-        std::cout 
+        std::cout
             << var.index()
-            << ", value " 
+            << ", value "
             << std::get<float>(var)
             << std::endl;
     }
@@ -71,7 +71,7 @@ namespace VariantDemo {
 
         std::variant<int, float, std::string> var{ std::string("Hello") };
 
-        std::cout 
+        std::cout
             << var.index()
             << ", value "
             << std::get<std::string>(var)
@@ -89,7 +89,7 @@ namespace VariantDemo {
 
         var = 123;
 
-        std::cout 
+        std::cout
             << var.index()
             << ", value "
             << std::get<int>(var)
@@ -98,6 +98,85 @@ namespace VariantDemo {
         if (const auto intPtr = std::get_if<0>(&var))
             std::cout << "int! => " << *intPtr << std::endl;
     }
+
+    void test_03() {
+
+        std::variant<int, float, std::string> var{ 3.5f };
+
+        // using a generic visitor (matching all types in the variant)
+        auto visitor = [](auto const& elem) { std::cout << elem << std::endl; };
+
+        std::visit(visitor, var);
+
+        var = 10;
+        std::visit(visitor, var);
+
+        var = std::string("Hello");
+        std::visit(visitor, var);
+    }
+
+    class MyVisitor
+    {
+    public:
+        MyVisitor() = default;
+
+        void operator() (int const& a) {
+            std::cout << "type is int" << std::endl;
+        }
+
+        void operator() (float const& a) {
+            std::cout << "type is float" << std::endl;
+        }
+
+        void operator() (std::string const& ) {
+            std::cout << "type is std::string" << std::endl;
+        }
+    };
+
+    void test_04() {
+
+        std::variant<int, float, std::string> var{ 3.5f };
+        std::visit(MyVisitor{}, var);
+        var = 10;
+        std::visit(MyVisitor{}, var);
+    }
+
+    void test_05() {
+
+        std::vector<std::variant<int, long, long long, float, double>>
+            vec = { 100, 200l, 300ll, 400.5f, 500.5 };
+
+        // display each value
+        std::cout << "Values:      ";
+        for (const auto& var : vec) {
+            std::visit([](auto& n) {std::cout << n << " "; }, var);
+        }
+        std::cout << std::endl;
+
+        // display each type
+        std::cout << "Types:       ";
+        for (const auto& var : vec) {
+            std::visit([](auto& arg) {std::cout << typeid(arg).name() << " "; }, var);
+        }
+        std::cout << std::endl;
+
+        // get the sum
+        std::common_type<int, long, long long, float, double>::type res{};
+        std::cout << "Type of Sum: " << typeid(res).name() << std::endl;
+
+        for (auto& var : vec) {
+            std::visit([&res](auto& arg) {res += arg; }, var);
+        }
+        std::cout << "Sum:         " << res << std::endl;
+
+        // double each value of the vector
+        std::cout << "Values:      ";
+        for (auto& var : vec) {
+            std::visit([](auto& arg) { arg *= 2; }, var);
+            std::visit([](auto& arg) { std::cout << arg << " "; }, var);
+        }
+        std::cout << std::endl;
+    }
 }
 
 void main_variant()
@@ -105,6 +184,9 @@ void main_variant()
     using namespace VariantDemo;
     test_01();
     test_02();
+    test_03();
+    test_04();
+    test_05();
 }
 
 // =====================================================================================
