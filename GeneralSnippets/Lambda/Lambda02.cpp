bc4f5ad8c49c545445db13935fb519b8d03273b6
Peter Loos <peter.loos@gmx.de>
2020-12-08 16:59:12 +0100

    minor improvements to 'lambda' example

--- GeneralSnippets/Lambda/Lambda02.cpp
+++ GeneralSnippets/Lambda/Lambda02.cpp
@@ -6,29 +6,30 @@
 
 namespace LambdaAndClosure {
 
-    void test_01() {
-
+    void test_01() 
+    {
         const int n{ 10 };
         auto lambda = [n](int a) {return n + a; };
         const auto m = lambda(20); // m is now 30
         std::cout << "m: " << m << std::endl;
     }
 
-    class ClosureClass
+    void test_02() 
     {
-    public:
-        ClosureClass(int n) : m_n(n) {}
-
-        int operator()(int a)
+        class ClosureClass
         {
-            return m_n + a;
-        }
+        public:
+            ClosureClass(int n) : m_n(n) {}
 
-    private:
-        int m_n;
-    };
+            // if lambda is 'mutable' remove const-ness
+            int operator()(int a) const
+            {
+                return m_n + a;
+            }
 
-    void test_02() {
+        private:
+            int m_n;
+        };
 
         const int n{ 30 };
         auto lambda = ClosureClass(n);
