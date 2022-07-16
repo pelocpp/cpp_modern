7d6e2f637afc554be0ea01312df41f810072b062
Peter Loos <peter.loos@gmx.de>
2022-07-16 19:02:15 +0200

    improved type traits

--- GeneralSnippets/TypeTraits/TypeTraits.cpp
+++ GeneralSnippets/TypeTraits/TypeTraits.cpp
@@ -7,8 +7,127 @@
 #include <list>
 #include <forward_list>
 #include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <iterator>
 
-namespace TypeTraits_Demo_Iterator
+namespace TypeTraits_Demo_Iterator_01
+{
+    void whichIterator(const std::input_iterator_tag)
+    {
+        std::cout << "[Input Iterator]" << std::endl;
+    }
+
+    void whichIterator(const std::output_iterator_tag)
+    {
+        std::cout << "[Output Iterator]" << std::endl;
+    }
+
+    void whichIterator(const std::forward_iterator_tag)
+    {
+        std::cout << "[Forward Iterator]" << std::endl;
+    }
+
+    void whichIterator(const std::bidirectional_iterator_tag)
+    {
+        std::cout << "[Bidirectional Iterator Iterator]" << std::endl;
+    }
+
+    void whichIterator(const std::random_access_iterator_tag)
+    {
+        std::cout << "[Random Access Iterator]" << std::endl;
+    }
+
+    template <typename TIterator>
+    auto getIteratorType(const TIterator&)
+    {
+        typename std::iterator_traits<TIterator>::iterator_category tag{};
+        return tag;
+    }
+
+    void test_1()
+    {
+        std::vector<int> vec;
+        whichIterator(getIteratorType(vec.begin()));
+
+        // oder:
+        std::vector<int>::iterator it;
+        whichIterator(getIteratorType(it));
+
+        // oder:
+        std::vector<int>::iterator::iterator_category iter_cat{};
+        whichIterator(iter_cat);
+    }
+
+    void test_2()
+    {
+        std::list<int> list;
+        whichIterator(getIteratorType(list.begin()));
+
+        // oder:
+        std::list<int>::iterator it;
+        whichIterator(getIteratorType(it));
+
+        // oder:
+        std::list<int>::iterator::iterator_category iter_cat{};
+        whichIterator(iter_cat);
+    }
+
+    void test_3()
+    {
+        std::forward_list<int> fwlist;
+        whichIterator(getIteratorType(fwlist.begin()));
+
+        // oder:
+        std::forward_list<int>::iterator it;
+        whichIterator(getIteratorType(it));
+
+        // oder:
+        std::forward_list<int>::iterator::iterator_category iter_cat{};
+        whichIterator(iter_cat);
+    }
+
+    void test_4()
+    {
+        std::ifstream source;
+        std::istream_iterator<int> input(source);
+        whichIterator(getIteratorType(input));
+
+        // oder:
+        std::istream_iterator<int> it{};
+        whichIterator(getIteratorType(it));
+
+        // oder:
+        std::istream_iterator<int>::iterator_category iter_cat{};
+        whichIterator(iter_cat);
+    }
+
+    void test_5()
+    {
+        std::ofstream source;
+        std::ostream_iterator<int> output(source);
+        whichIterator(getIteratorType(output));
+
+        // oder:
+        // std::ostream_iterator<int> has no default c'tor
+
+        // oder:
+        std::ostream_iterator<int>::iterator_category iter_cat{};
+        whichIterator(iter_cat);
+    }
+
+    void test_01()
+    {
+        test_1();
+        test_2();
+        test_3();
+        test_4();
+        test_5();
+    }
+}
+
+namespace TypeTraits_Demo_Iterator_02
 {
     template <typename Iterator>
     using ValueType = typename Iterator::value_type;
@@ -51,7 +170,7 @@ namespace TypeTraits_Demo_Iterator
         return getAt(it, size, category);
     }
 
-    void test_01() 
+    void test_02() 
     {
         std::forward_list<char> charList{ 'A', 'B', 'C', 'D', 'E' };
         char ch = getAt(charList.begin(), 3);
@@ -67,7 +186,7 @@ namespace TypeTraits_Demo_Iterator
     }
 }
 
-namespace TypeTraits_Demo_Remove_Reference
+namespace TypeTraits_Demo_Remove_Reference_03
 {
     template<typename Container>
     void sort_01(Container& container)
@@ -119,7 +238,7 @@ namespace TypeTraits_Demo_Remove_Reference
         );
     }
 
-    void test_02()
+    void test_03()
     {
         std::vector<int> vec{ 5, 4, 6, 3, 7, 2, 8, 1, 9 };
 
@@ -132,17 +251,19 @@ namespace TypeTraits_Demo_Remove_Reference
             std::cout << n << ' ';
         }
         std::cout << std::endl;
-
     }
 }
 
 void main_type_traits()
 {
-    using namespace TypeTraits_Demo_Iterator;
+    using namespace TypeTraits_Demo_Iterator_01;
     test_01();
 
-    using namespace TypeTraits_Demo_Remove_Reference;
+    using namespace TypeTraits_Demo_Iterator_02;
     test_02();
+
+    using namespace TypeTraits_Demo_Remove_Reference_03;
+    test_03();
 }
 
 // =====================================================================================
