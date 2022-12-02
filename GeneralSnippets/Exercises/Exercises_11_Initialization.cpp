e6f43cfe3add7be917c3e60c3fed6bfd606cabc9
Peter Loos <peter.loos@gmx.de>
2022-12-02 18:34:06 +0100

    added one more exercise

--- GeneralSnippets/Exercises/Exercises_11_Initialization.cpp
+++ GeneralSnippets/Exercises/Exercises_11_Initialization.cpp
@@ -57,12 +57,93 @@ namespace Exercises_Initialization {
             container6();
         }
     }
+
+    namespace Exercise_02 {
+
+        void printResults(
+            std::string tag,
+            std::chrono::high_resolution_clock::time_point startTime,
+            std::chrono::high_resolution_clock::time_point endTime)
+        {
+            std::cout
+                << tag
+                << std::fixed
+                << std::setprecision(6)
+                << std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(endTime - startTime).count()
+                << " msecs."
+                << std::endl;
+        }
+
+        class SomeStrings
+        {
+        public:
+            static void transferVector(const std::vector<std::string>& strings)
+            {
+                // prevent optimizer to work to hard ...
+                auto& first = strings.front();
+                auto& last = strings.back();
+                //std::cout << "first=" << first << std::endl;
+                //std::cout << "last=" << last << std::endl;
+            }
+
+            static void transferVectorList(const std::initializer_list<std::string> strings)
+            {
+                // prevent optimizer to work to hard ...
+                const auto& first = strings.begin();
+                const auto& last = std::prev(strings.end());
+                //std::cout << "first=" << *first << std::endl;
+                //std::cout << "last=" << *last << std::endl;
+            }
+        };
+
+        // Note: Debug vs Release Mode
+        constexpr int MaxIteration = 10'000;
+
+        void testExercise_02() {
+            {
+                const auto startTime{ std::chrono::high_resolution_clock::now() };
+
+                for (size_t i{}; i != MaxIteration; ++i) {
+
+                    // heap-based transfer of strings
+                    SomeStrings::transferVector(
+                        std::vector<std::string> { 
+                        "A", "B", "C", "D", "E", "F", "G", "H", "I",
+                        "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+                        "S", "T", "U", "V", "W", "X", "Y", "Z"
+                    });
+                }
+
+                const auto endTime{ std::chrono::high_resolution_clock::now() };
+                printResults("std::vector:           ", startTime, endTime);
+            }
+
+            {
+                const auto startTime{ std::chrono::high_resolution_clock::now() };
+
+                for (size_t i{}; i != MaxIteration; ++i) {
+
+                    // stack-based transfer of strings
+                    SomeStrings::transferVectorList(
+                        std::initializer_list<std::string> {
+                        "A", "B", "C", "D", "E", "F", "G", "H", "I",
+                        "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+                        "S", "T", "U", "V", "W", "X", "Y", "Z"
+                    });
+                }
+
+                const auto endTime{ std::chrono::high_resolution_clock::now() };
+                printResults("std::initializer_list: ", startTime, endTime);
+            }
+        }
+    }
 }
 
 void test_exercises_initialization()
 {
     using namespace Exercises_Initialization;
-    Exercise_01::testExercise_01();
+    // Exercise_01::testExercise_01();
+    Exercise_02::testExercise_02();
 }
 
 // =====================================================================================
