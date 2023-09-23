18857cfaff79ff973b822e1139b0531b3a1382c3
Peter Loos <peter.loos@gmx.de>
2023-09-23 17:43:38 +0200

    added brace initialization

--- GeneralSnippets/Literals/Literals.cpp
+++ GeneralSnippets/Literals/Literals.cpp
@@ -15,10 +15,10 @@ namespace Literals_With_Separators {
         // binary, octal and hexadecimal literals
         // (including single quotation mark as separator)
 
-        long decval = 1'048'576;       // groups of three digits
-        long hexval = 0x10'0000;       // four digits
-        long octval = 00'04'00'00'00;  // two digits
-        long binval = 0b1'00000000'00000000'00000000;  // eight digits
+        long decval { 1'048'576 };
+        long hexval { 0x10'0000 };
+        long octval { 00'04'00'00'00 };
+        long binval { 0b1'00000000'00000000'00000000 };
 
         std::cout << decval << std::endl;
         std::cout << hexval << std::endl;
@@ -77,6 +77,7 @@ namespace Literals_Color_Runtime {
 
         // tiny implementation - just parsing hexadecimal format
         std::string arg(literal);
+
         if (arg.size() == 2 /* 0x */ + 6 /* FF FF FF */) {
 
             std::string rs { arg.substr(2, 2) };
@@ -87,34 +88,38 @@ namespace Literals_Color_Runtime {
             uint8_t g { static_cast<uint8_t>(std::stoi(gs, nullptr, 16)) };
             uint8_t b { static_cast<uint8_t>(std::stoi(bs, nullptr, 16)) };
 
-            return Color { r, g, b };
+            return Color{ r, g, b };
         }
 
-        return Color {};
+        return Color{};
     }
 
     void test_02() {
-        Color red = 0xFF0000_rgb;
+
+        Color red{ 0xFF0000_rgb };
         std::cout << red << std::endl;
-        Color magenta = 0xFF00FF_rgb;
+
+        Color magenta{ 0xFF00FF_rgb };
         std::cout << magenta << std::endl;
-        Color yellow = 0xFFFF00_rgb;
+
+        Color yellow{ 0xFFFF00_rgb };
         std::cout << yellow << std::endl;
 
-        Color unknown = 12345_rgb;
+        Color unknown{ 12345_rgb };
         std::cout << unknown << std::endl;
 
         unknown = "0x012345"_rgb;
         std::cout << unknown << std::endl;
     }
 
-    void test_02_with_errors() {
+    void test_02_with_errors() 
+    {
         // value outside rgb range
-         Color col1 = 0x1FFFFFF_rgb;
+         Color col1{ 0x1FFFFFF_rgb };
          std::cout << col1 << std::endl;
 
         // illegal hexadecimal digit
-         Color col2 = "0x00GG00"_rgb;
+         Color col2{ "0x00GG00"_rgb };
          std::cout << col2 << std::endl;
     }
 }
@@ -161,11 +166,11 @@ namespace Literals_Color_CompileTime {
         uint8_t g{ static_cast<uint8_t>((value & 0x0000FF00) >> 8) };
         uint8_t b{ static_cast<uint8_t>((value & 0x000000FF) >> 0) };
 
-        return Color { r, g, b };
+        return Color{ r, g, b };
     }
 
-    constexpr bool isHex(char ch)
-    {
+    constexpr bool isHex(char ch) {
+
         if ((ch >= '0' && ch <= '9') || (ch >= 'A' && ch <= 'F') || (ch >= 'a' && ch <= 'f')) {
             return true;
         }
@@ -173,14 +178,15 @@ namespace Literals_Color_CompileTime {
         return false;
     }
 
-    constexpr uint8_t hex2int(char ch)
-    {
+    constexpr uint8_t hex2int(char ch) {
+
         if (!isHex(ch)) {
             throw std::logic_error("illegal hexadecimal digit");
         }
 
         // transform hex character to 4-bit equivalent number
-        uint8_t byte = ch;
+        uint8_t byte{ static_cast<uint8_t>(ch) };
+
         if (byte >= '0' and byte <= '9') {
             byte -= '0';
         }
@@ -190,20 +196,24 @@ namespace Literals_Color_CompileTime {
         else if (byte >= 'A' and byte <= 'F') {
             byte -= ('A' - 10);
         }
+
         return byte;
     }
 
     constexpr size_t hexstoi(const char* str)
     {
         int value{};
+
         while (*str != '\0') {
+
             // get current character, then increment
-            uint8_t byte = hex2int(*str);
+            uint8_t byte{ hex2int(*str) };
             ++str;
 
             // shift 4 to make space for new digit, and add the 4 bits of the new digit 
             value = (value << 4) | (byte & 0xF);
         }
+
         return value;
     }
 
@@ -212,6 +222,7 @@ namespace Literals_Color_CompileTime {
 
         // std::string is partially 'constexpr'
         std::string arg(literal);
+
         if (arg.size() == 2 /* 0x */ + 6 /* FF FF FF */) {
 
             std::string rs{ arg.substr(2, 2) };
@@ -219,35 +230,37 @@ namespace Literals_Color_CompileTime {
             std::string bs{ arg.substr(6, 2) };
 
             // std::stoi is not 'constexpr' -  at the time of this writing
-             uint8_t r = static_cast<uint8_t>(hexstoi(rs.c_str()));
-             uint8_t g = static_cast<uint8_t>(hexstoi(gs.c_str()));
-             uint8_t b = static_cast<uint8_t>(hexstoi(bs.c_str()));
+             uint8_t r { static_cast<uint8_t>(hexstoi(rs.c_str())) };
+             uint8_t g { static_cast<uint8_t>(hexstoi(gs.c_str())) };
+             uint8_t b { static_cast<uint8_t>(hexstoi(bs.c_str())) };
 
-            return Color { r, g, b };
+            return Color{ r, g, b };
         }
 
-        return Color {};
+        return Color{};
     }
 
     void test_03() {
-        constexpr Color red = 0xFF0000_rgb;
+        constexpr Color red{ 0xFF0000_rgb };
         std::cout << red << std::endl;
-        constexpr Color magenta = 0xFF00FF_rgb;
+
+        constexpr Color magenta{ 0xFF00FF_rgb };
         std::cout << magenta << std::endl;
-        constexpr Color yellow = 0xFFFF00_rgb;
+
+        constexpr Color yellow{ 0xFFFF00_rgb };
         std::cout << yellow << std::endl;
 
-        constexpr Color unknown = 12345_rgb;
+        constexpr Color unknown{ 12345_rgb };
         std::cout << unknown << std::endl;
     }
 
     // throws errors at compile time
     void test_03_with_errors() {
         // value outside rgb range
-        // constexpr Color col1 = 0x1FFFFFF_rgb;
+        // constexpr Color col1{ 0x1FFFFFF_rgb };
 
         // illegal hexadecimal digit
-        // constexpr Color col2 = "0x00GG00"_rgb;
+        // constexpr Color col2{ "0x00GG00"_rgb };
     }
 }
 
