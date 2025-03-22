88544a7a2cfb9f37ab49e6b3e04d451bc960de76
Peter Loos <peter.loos@gmx.de>
2025-03-22 18:53:15 +0100

    continued work

--- GeneralSnippets/Casts/Casts.cpp
+++ GeneralSnippets/Casts/Casts.cpp
@@ -8,7 +8,118 @@ namespace DiverseCasts {
 
     namespace CastStatic {
 
-        static void test_00() {
+        static void test_01_implicit_conversions()
+        {
+            {
+                // no conversions at all
+                int a = 123;
+                long b = 123l;
+            }
+
+            {
+                // implicit conversions, done by the compiler
+                long a = 123;      // int implicitly converted to long 
+                double b = 123l; // long implicitly converted to double
+            }
+
+            {
+                // implicit conversions, done by the machine code 
+                int a = 123;    
+                long b = 123l; 
+
+                long n = a;      // int implicitly converted to long 
+                double m = b; // long implicitly converted to double
+            }
+
+            {
+                // Promotion
+                long   a = 123;   // int promoted to long
+                double b = 123l;  // long promoted to double
+            }
+
+            {
+                // Demotion
+                int  a = 10.5;   // warning: possible loss of data
+                bool b = 123;    // warning: possible loss of data
+            }
+        }
+
+        static void test_02_explicit_conversions_static_cast()
+        {
+            {
+                // static_cast
+                int  a = static_cast<int>(123.456);  // double demoted to int
+            }
+
+            {
+                // C-Style-Cast
+                char c = 10;        // 1 byte
+                int* p = (int*) &c; // 4 bytes, works ?!?
+
+                // *p = 5;            // run-time error: stack corruption
+            }
+
+            {
+                // static_cast
+                //char c = 10;                    // 1 byte
+                //int* p = static_cast<int*>(&c); // 4 bytes // compile error
+            }
+        }
+
+        static void test_03_explicit_conversions_reinterpret_cast()
+        {
+            {
+                // reinterpret_cast
+                char c = 10;                           // 1 byte
+                int* p = reinterpret_cast<int*>(&c);   // 4 bytes // compiles
+            }
+
+            struct MyStruct
+            {
+                char x1;
+                char x2;
+                char x3;
+                char x4;
+            };
+
+            {
+                struct MyStruct s{ 'A' , 'B' , 'C' , '\0' };
+
+                // reinterpret struct as char* pointer :-) 
+                char* ptr = reinterpret_cast<char*>(&s);
+
+                std::println("{}", ptr);
+            }
+        }
+
+        static void print(int* ptr)
+        {
+            std::println("{}", *ptr);
+        }
+
+        static void test_04_explicit_conversions_const_cast()
+        {
+            {
+                const int constVar = 123;
+                int* nonConstIp = const_cast<int*>(&constVar); // removes const
+                *nonConstIp = 10; // potential run-time error
+            }
+
+            {
+                const int constVar = 123;
+
+                // print(&constVar);   // error: cannot convert argument 1 from 'const int*' to 'int*'
+
+                print(const_cast<int*>(&constVar)); // allowed
+            }
+        }
+
+
+        // altes Zeugs
+
+
+        static void test_00()
+        {
             long long l = 1l;            // 8 Byte
             int  n = 2;                  // 4 Byte
             n = (int)l;
@@ -168,25 +279,25 @@ namespace DiverseCasts {
             delete p;
         }
 
-        struct myStruct {
-            char x1;
-            char x2;
-            char x3;
-            char x4;
-        };
+        //struct myStruct {
+        //    char x1;
+        //    char x2;
+        //    char x3;
+        //    char x4;
+        //};
 
-        static void test_06() {
+        //static void test_06() {
 
-            using namespace CastReinterpret;
+        //    using namespace CastReinterpret;
 
-            // using {} initialization syntax
-            myStruct s{ 'A' , 'B' , 'C' , '\0' };
+        //    // using {} initialization syntax
+        //    myStruct s{ 'A' , 'B' , 'C' , '\0' };
 
-            // reinterpret struct as char* pointer :-) 
-            char* ptr = reinterpret_cast<char*>(&s);
+        //    // reinterpret struct as char* pointer :-) 
+        //    char* ptr = reinterpret_cast<char*>(&s);
 
-            std::cout << ptr << std::endl;
-        }
+        //    std::cout << ptr << std::endl;
+        //}
     }
 }
 
@@ -197,15 +308,217 @@ void main_casts()
     using namespace DiverseCasts::CastConst;
     using namespace DiverseCasts::CastReinterpret;
 
-    test_00();
-    test_01();
-    test_02();
-    test_03();
-    test_04();
-    test_05();
-    test_06();
+    test_01_implicit_conversions();
+    test_02_explicit_conversions_static_cast();
+    test_03_explicit_conversions_reinterpret_cast();
+    test_04_explicit_conversions_const_cast();
+
+
 }
 
 // =====================================================================================
 // End-of-File
 // =====================================================================================
+
+//
+//namespace DiverseCasts {
+//
+//    namespace CastStatic {
+//
+//        static void test_00() {
+//            long long l = 1l;            // 8 Byte
+//            int  n = 2;                  // 4 Byte
+//            n = (int)l;
+//            n = static_cast<int> (l);
+//
+//            double d = 1.0;              // 8 Byte
+//            float  f = 2.0f;             // 4 Byte
+//            f = (float)d;                // Note: ==> Go to Disassembly (cvtsd2ss)
+//            f = static_cast<float> (d);
+//
+//            // convert Scalar Double-Precision Floating-Point Value to 
+//            // Scalar Single-Precision Floating-Point Value
+//        }
+//
+//        // -------------------------------------------------------------------
+//
+//        enum class Color { red, green, blue };
+//
+//        static void test_01() {
+//
+//            // int n = Color::blue;  // a value of type "Color" cannot be used to initialize an entity of type "int"
+//
+//            int m = static_cast<int>(Color::blue);
+//            std::cout << m << std::endl;
+//        }
+//
+//        // -------------------------------------------------------------------
+//
+//        struct Base {
+//            // int m = 0;
+//            void sayHello() const {
+//                std::cout << "This is Base!" << std::endl;
+//            }
+//        };
+//
+//        struct Derived : Base {
+//            void sayHello() const {
+//                std::cout << "This is Derived!" << std::endl;
+//            }
+//        };
+//
+//        // static_cast
+//        static void test_02() {
+//
+//            using namespace CastStatic;
+//
+//            // upcast
+//            Derived derived;
+//            Base& br = derived; // upcast: implicit conversion - allowed
+//            br.sayHello();
+//
+//            // downcast
+//            Derived& anotherDerived = (Derived&)br; // downcast - old-style cast
+//            anotherDerived.sayHello();
+//
+//            Derived& anotherDerived2 = static_cast<Derived&>(br); // downcast - correct style
+//            anotherDerived2.sayHello();
+//
+//            Base anotherBase;
+//            Derived& anotherDerived3 = (Derived&)anotherBase; // downcast - old-style cast
+//            anotherDerived3.sayHello();
+//
+//            Derived& anotherDerived4 = static_cast<Derived&>(anotherBase); // downcast - correct style
+//            anotherDerived4.sayHello();
+//        }
+//    }
+//
+//    // -------------------------------------------------------------------
+//
+//    namespace CastDynamic {
+//
+//        struct Base {
+//            // int m = 0;
+//            virtual void sayHello() const {
+//                std::cout << "This is Base!" << std::endl;
+//            }
+//        };
+//
+//        struct Derived : Base {
+//            void sayHello() const override {
+//                std::cout << "This is Derived!" << std::endl;
+//            }
+//        };
+//
+//        Derived* testFunc(Base* baseRef) {
+//
+//            Derived* pDerived = dynamic_cast<Derived*> (baseRef);
+//            if (pDerived != nullptr) {
+//                pDerived->sayHello();
+//            }
+//            else {
+//                std::cout << "dynamic_cast failed!" << std::endl;
+//            }
+//
+//            return static_cast<Derived*>(baseRef);
+//        }
+//
+//        static void test_03() {
+//
+//            using namespace CastDynamic;
+//
+//            Base aBase;
+//            Derived aDerived;
+//
+//            Base* pBase1 = &aBase;
+//            Base* pBase2 = &aDerived;
+//
+//            Derived* pResult;
+//
+//            pResult = testFunc(pBase2);
+//            pResult->sayHello();
+//
+//            pResult = testFunc(pBase1);
+//            pResult->sayHello();
+//        }
+//    }
+//
+//    // -------------------------------------------------------------------
+//
+//    namespace CastConst {
+//
+//        static void test_04() {
+//
+//            int n = 1;
+//            const int* ip1 = &n;
+//            std::cout << n << std::endl;
+//
+//            // *ip1 = 2;  // doesn't compile
+//
+//            // remove constness from pointer
+//            int* ip2 = const_cast <int*> (ip1);
+//            *ip2 = 2;  // compiles :-)
+//            std::cout << n << std::endl;
+//
+//            // remove constness from a reference
+//            const int& ri = n;
+//            const_cast <int&> (ri) = 3;
+//            std::cout << n << std::endl;
+//
+//            // convert const pointer directly
+//            *const_cast <int*> (ip1) = 4;
+//            std::cout << n << std::endl;
+//        }
+//    }
+//
+//    // -------------------------------------------------------------------
+//
+//    namespace CastReinterpret {
+//
+//        static void test_05() {
+//
+//            int* p = new int(65);
+//            char* ch = reinterpret_cast<char*>(p);
+//            std::cout << *p << std::endl;
+//            std::cout << *ch << std::endl;
+//
+//            delete p;
+//        }
+//
+//        struct myStruct {
+//            char x1;
+//            char x2;
+//            char x3;
+//            char x4;
+//        };
+//
+//        static void test_06() {
+//
+//            using namespace CastReinterpret;
+//
+//            // using {} initialization syntax
+//            myStruct s{ 'A' , 'B' , 'C' , '\0' };
+//
+//            // reinterpret struct as char* pointer :-) 
+//            char* ptr = reinterpret_cast<char*>(&s);
+//
+//            std::cout << ptr << std::endl;
+//        }
+//    }
+//}
+//
+//void main_casts()
+//{
+//    using namespace DiverseCasts::CastStatic;
+//    using namespace DiverseCasts::CastDynamic;
+//    using namespace DiverseCasts::CastConst;
+//    using namespace DiverseCasts::CastReinterpret;
+//
+//    test_00();
+//    test_01();
+//    test_02();
+//    test_03();
+//    test_04();
+//    test_05();
+//    test_06();
+//}
--- GeneralSnippets/Casts/Casts.md
+++ GeneralSnippets/Casts/Casts.md
@@ -12,16 +12,23 @@
 
   * [Einleitung](#link1)
   * [Implizite Konvertierungen](#link2)
-  * [Explizite C-Style Konvertierungen](#link3)
+  * [Explizite Konvertierungen: C-Style](#link3)
   * [Explizite C++ Konvertierungen](#link4)
+  * [Die `static_cast`-Operation](#link5)
+  * [Die `reinterpret_cast`-Operation](#link6)
+  * [Die `const_cast`-Operation](#link7)
+  * [*C-Style-Casts* und *New-Style-Casts*](#link8)
+  * [Die `dynamic_cast`-Operation](#link9)
+  * [Literatur](#link10)
+
 
 ---
 
 ## Einleitung <a name="link1"></a>
 
-<img src="cpp_casts.svg" width="500">
+<img src="cpp_casts.svg" width="450">
 
-*Abbildung* 1: Überblick übert die unterschiedlichen Typkonvertierungsmöglichkeiten in C/C++
+*Abbildung* 1: Überblick über die unterschiedlichen Typkonvertierungsmöglichkeiten in C/C++
 
 ---
 
@@ -38,6 +45,284 @@ https://stackoverflow.com/questions/103512/why-use-static-casttx-instead-of-tx
 
 
 ---
+
+## Implizite Konvertierungen <a name="link2"></a>
+
+Eine implizite Konvertierung wird vom Compiler automatisch durchgeführt, wenn ein Ausdruck in einen seiner kompatiblen Typen konvertiert werden muss.
+
+Beispielsweise können Konvertierungen zwischen primitiven Datentypen implizit erfolgen.
+
+```cpp
+01: long a = 5;      // int implicitly converted to long 
+02: double b = 123l; // long implicitly converted to double
+```
+
+*Maschinencode*:
+
+```asm
+                long a = 5;      // int implicitly converted to long 
+00007FF67026C74A  mov         dword ptr [rbp+44h],5  
+                double b = 123l; // long implicitly converted to double
+00007FF67026C751  movsd       xmm0,mmword ptr [__real@405ec00000000000 (07FF670503070h)]  
+00007FF67026C759  movsd       mmword ptr [rbp+68h],xmm0
+```
+
+Implizite primitive Konvertierungen können auch zur Laufzeit durchgeführt werden:
+
+```cpp
+```
+
+*Maschinencode*:
+
+```asm
+                int a = 5;    
+00007FF67026C75E  mov         dword ptr [rbp+84h],5  
+                long b = 123l; 
+00007FF67026C768  mov         dword ptr [rbp+0A4h],7Bh  
+
+                // implicit conversions, done by the machine code
+                long n = a;      // int implicitly converted to long 
+00007FF67026C772  mov         eax,dword ptr [rbp+84h]  
+00007FF67026C778  mov         dword ptr [rbp+0C4h],eax  
+                double m = b; // long implicitly converted to double
+00007FF67026C77E  cvtsi2sd    xmm0,dword ptr [rbp+0A4h]  
+00007FF67026C786  movsd       mmword ptr [rbp+0E8h],xmm0
+```
+
+Hier erkennen wir, dass beispielsweise bei der Konvertierung von ganzzahligen Werten nach Gleitpunktwerten
+Konvertierungsroutinen zur Laufzeit ausgeführt werden müssen. Dies kostet Laufzeit!
+
+Diese impliziten primitiven Konvertierungen lassen sich weiter in zwei Arten unterteilen:
+*Promotion* und *Demotion* (in etwa &bdquo;*Heraufstufung*&bdquo; und &bdquo;*Herabstufung*&bdquo;).
+
+Eine *Promotion* erfolgt, wenn ein Ausdruck implizit in einen größeren Typ konvertiert wird,
+und eine *Demotion* erfolgt, wenn ein Ausdruck in einen kleineren Typ konvertiert wird.
+
+```cpp
+01: // Promotion
+02: long   a = 123;   // int promoted to long
+03: double b = 123l;  // long promoted to double
+```
+
+Da eine *Demotion* zu Informationsverlust führen kann, erzeugen diese Konvertierungen
+bei den meisten Compilern eine Warnung:
+
+```cpp
+01: // Demotion
+02: int  a = 10.5;   // warning: possible loss of data
+03: bool b = 123;    // warning: possible loss of data
+```
+
+*Output-Window*:
+
+```
+warning C4244: 'initializing': conversion from 'double' to 'int', possible loss of data
+warning C4305: 'initializing': truncation from 'int' to 'bool'
+```
+
+Wenn der potenzielle Informationsverlust beabsichtigt ist,
+kann man die Warnung durch eine &bdquo;*explizite*&rdquo; Konvertierung unterdrücken.
+
+Dies führt uns zum nächsten Abschnitt:
+
+## Explizite Konvertierungen: C-Style <a name="link3"></a>
+
+Die erste explizite Konvertierung (im engl. &bdquo;*Type Cast*&rdquo;)
+ist der aus C übernommene und wird allgemein als *C-Style-Cast* bezeichnet.
+
+Der gewünschte Datentyp wird einfach in Klammern links neben dem zu konvertierenden Ausdruck platziert:
+
+```cpp
+01: // C-style cast
+02: int  a = (int) 123.456;  // double demoted to int
+03: bool b = (bool) 123;     // int demoted to char
+```
+
+
+Der *C-Style-Cast* eignet sich für die meisten Konvertierungen zwischen primitiven Datentypen.
+
+Bei Konvertierungen zwischen Klassen und Zeigern kann er jedoch zu mächtig sein.
+
+Um eine bessere Kontrolle über die verschiedenen möglichen Konvertierungsarten zu erhalten,
+wurden in C++ vier neue Casts eingeführt, die sogenannten *Named Casts* oder *New-Style-Casts*.
+
+
+
+## Explizite C++ Konvertierungen <a name="link4"></a>
+
+Die Namen der vier *New-Style-Casts* lauten 
+
+```cpp
+static_cast<new_type> (expression) 
+reinterpret_cast<new_type> (expression) 
+const_cast<new_type> (expression) 
+dynamic_cast<new_type> (expression)
+```
+
+Wie zu sehen ist, folgt auf den Namen des Casts der neue Typ in spitzen Klammern
+und anschließend der zu konvertierende Ausdruck in Klammern.
+
+Diese Casts ermöglichen eine präzisere Kontrolle über die Konvertierung,
+was wiederum dem Compiler das Erkennen von Konvertierungsfehlern erleichtert.
+
+*Bemerkung*:<br />
+Der *C-Style-Cast* umfasst in einer Definition den `static_cast`, den `reinterpret_cast`
+und den `const_cast`.
+Bei falscher Anwendung führt dieser Cast daher häufiger zu subtilen Konvertierungsfehlern.
+
+### Die `static_cast`-Operation <a name="link5"></a>
+
+Die `static_cast`-Operation führt Konvertierungen zwischen kompatiblen Typen durch.
+
+Er ähnelt dem *C-Style-Cast*, ist aber restriktiver.
+
+Beispielsweise erlaubt der *C-Style-Cast*, dass ein Integer-Zeiger auf eine `char`-Variable zeigt:
+
+```cpp
+// C-Style-Cast
+char c = 10;       // 1 byte
+int* p = (int*)&c; // 4 bytes, compikles, works ?!?
+```
+
+Da dies zu einem 4-Byte-Zeiger führt, der auf 1 Byte allokierten Speicher verweist,
+führt das Schreiben in diesen Zeiger entweder zu einem Laufzeitfehler oder überschreibt angrenzenden Speicher:
+
+```cpp
+*p = 5;            // run-time error: stack corruption
+```
+
+Im Gegensatz zum Cast im *C-Style* ermöglicht der statische Cast dem Compiler, zu überprüfen,
+ob die *Pointer*- und *Pointee*-Datentypen kompatibel sind,
+wodurch der Programmierer diese falsche Zeigerzuweisung während der Kompilierung erkennen kann:
+
+```cpp
+01: // static_cast
+02: char c = 10;                    // 1 byte
+03: int* p = static_cast<int*>(&c); // 4 bytes
+```
+
+Der Compiler reagiert mit der Fehlermeldung <i>Invalid type conversion: 'static_cast': cannot convert from 'char *' to 'int *'</i>.
+
+### Die `reinterpret_cast`-Operation <a name="link6"></a>
+
+Um eine Zeigerkonvertierung zu erzwingen, verwendet man stattdessen die `reinterpret_cast`-Operation.
+Dieser arbeitet im Hintergrund auf dieselbe Weise wie der *C-Style-Cast*:
+
+```cpp
+01: // reinterpret_cast
+02: char c = 10;                           // 1 byte
+03: int* p = reinterpret_cast<int*>(&c);   // 4 bytes // compiles
+```
+
+Die `reinterpret_cast`-Operation verarbeitet Konvertierungen zwischen bestimmten, nicht verwandten Typen, beispielsweise von einem Zeigertyp in einen anderen, inkompatiblen Zeigertyp.
+
+Er führt lediglich eine binäre Kopie der Daten durch, ohne das zugrunde liegende Bitmuster zu verändern oder zu interpretieren.
+
+Beachten Sie, dass das Ergebnis einer solchen Low-Level-Operation systemspezifisch und daher nicht portierbar ist.
+
+Diese Cast-Operation  sollte mit Vorsicht verwendet werden, wenn sie sich nicht vollständig vermeiden lässt.
+
+
+Ein zweites Beispiel:
+
+```cpp
+01: struct MyStruct
+02: {
+03:     char x1;
+04:     char x2;
+05:     char x3;
+06:     char x4;
+07: };
+08: 
+09: {
+10:     struct MyStruct s{ 'A' , 'B' , 'C' , '\0' };
+11: 
+12:     // reinterpret struct as char* pointer :-) 
+13:     char* ptr = reinterpret_cast<char*>(&s);
+14: 
+15:     std::println("{}", ptr);
+16: }
+```
+
+*Ausgabe*:
+
+```
+ABC
+```
+
+
+### Die `const_cast`-Operation <a name="link7"></a>
+
+Der dritte C++-Cast ist die `const_cast`-Operation.
+Sie wird hauptsächlich verwendet, um den `const`-Modifikator einer Variablen hinzuzufügen oder zu entfernen.
+
+```cpp
+01: const int constVar = 123;
+02: int* nonConstIp = const_cast<int*>(&constVar); // removes const
+```
+
+Obwohl die `const_cast`-Operation die Änderung des Wertes einer Konstanten ermöglicht,
+handelt es sich dabei immer noch um ungültigen Code, der einen Laufzeitfehler verursachen kann.
+
+Dies könnte beispielsweise der Fall sein, wenn sich die Konstante in einem Bereich des *Read-Only*-Speichers befindet:
+
+```cpp
+*nonConstIp = 10; // potential run-time error
+```
+
+Die `const_cast`-Operation wird häufig dann verwendet,
+wenn eine Funktion ein nicht konstantes Zeigerargument annimmt, obwohl sie den Zeiger nicht ändert:
+
+```cpp
+01: void print(int* ptr)
+02: {
+03:     std::println("{}", *ptr);
+04: }
+```
+
+Der Funktion kann dann mithilfe einer  `const_cast`-Operation eine konstante Variable übergeben werden:
+
+```cpp
+01: const int constVar = 123;
+02: 
+03: // print(&constVar);                  // error: cannot convert argument 1 from 'const int*' to 'int*'
+04: 
+05: print(const_cast<int*>(&constVar));   // compiles, runs
+```
+
+---
+
+### *C-Style-Casts* und *New-Style-Casts* <a name="link8"></a>
+
+Beachten Sie, dass der *C-Style-Cast* den `const`-Modifikator ebenfalls entfernen kann.
+Da diese Konvertierung jedoch im Hintergrund erfolgt, sind *New-Style-Casts* vorzuziehen.
+
+Ein weiterer Grund für die Verwendung von *New-Style-Casts* ist,
+dass sie im Quellcode leichter zu finden sind als ein *C-Style-Cast*.
+
+Das ist wichtig, da Typkonvertierungsfehler schwer zu entdecken sein können.
+
+Ein dritter Grund für die Verwendung von *C-Style-Casts* ist deren unkomfortable Schreibweise.
+
+Da explizite Konvertierungen in vielen Fällen vermieden werden können, wurde dies bewusst so gewählt,
+dass Entwickler nach einer anderen Lösung suchen.
+
+---
+
+### Die `dynamic_cast`-Operation <a name="link9"></a>
+
+WEITER:
+
+https://web.archive.org/web/20160316114647/http://pvtuts.com/cpp/cpp-type-conversions
+
+
+---
+
+## Literatur <a name="link10"></a>
+
+
+---
+
 
 ALTES ZEUGS
 
