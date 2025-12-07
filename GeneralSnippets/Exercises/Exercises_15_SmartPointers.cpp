057f835e4b4b7cbff52183206f72fd30a1bdd713
Peter Loos <peter.loos@gmx.de>
2025-12-07 11:28:46 +0100

    added new exercise

--- GeneralSnippets/Exercises/Exercises_15_SmartPointers.cpp
+++ GeneralSnippets/Exercises/Exercises_15_SmartPointers.cpp
@@ -26,7 +26,7 @@ namespace Exercises_SmartPointers {
         // =============================================================
         // Counting references :) 
 
-        static void testExercise_01() {
+        static void testExercise() {
 
             class X {};
 
@@ -59,7 +59,7 @@ namespace Exercises_SmartPointers {
         // =============================================================
         // Don't double-manage!
 
-        static void testExercise_02() {
+        static void testExercise() {
 
             class X
             {
@@ -124,7 +124,7 @@ namespace Exercises_SmartPointers {
             }
         };
 
-        static void testExercise_03a()
+        static void testExercise_01()
         {
             UnsafeWatcher watcher;
 
@@ -155,7 +155,7 @@ namespace Exercises_SmartPointers {
             }
         };
 
-        static void testExercise_03b()
+        static void testExercise_02()
         {
             HeavyAndSafeWatcher watcher;
 
@@ -195,7 +195,7 @@ namespace Exercises_SmartPointers {
             }
         };
 
-        static void testExercise_03c() {
+        static void testExercise_03() {
 
             LightweightAndSafeWatcher watcher;
 
@@ -213,10 +213,351 @@ namespace Exercises_SmartPointers {
             }
         }
 
-        static void testExercise_03() {
-            testExercise_03a();
-            testExercise_03b();
-            testExercise_03c();
+        static void testExercise() {
+            testExercise_01();
+            testExercise_02();
+            testExercise_03();
+        }
+    }
+
+    namespace Exercise_04 {
+
+        // =============================================================
+        // MyString implementation using std::shared_ptr
+
+        class MyStringSP
+        {
+        private:
+            std::shared_ptr<char[]> m_string;
+            std::size_t             m_length;
+
+        public:
+            // default c'tor
+            MyStringSP() : m_length{} {}
+
+            // user defined c'tor
+            MyStringSP(const char* string)
+            {
+                m_length = std::strlen(string);
+                m_string = std::make_shared<char[]>(m_length + 1);
+                std::memcpy(m_string.get(), string, m_length + 1);  // incl. copying '\0'
+
+            }
+
+            // no destructor necessary
+
+            // public interface
+            void print()
+            {
+                if (m_string.get() == nullptr) {
+                    std::println("<null>");
+                }
+                else {
+                    std::println("{}", m_string.get());
+                }
+            }
+
+            // getter
+            std::size_t length() const { return m_length; }
+
+            // subscript operator
+            char& operator[] (int index) {
+
+                if (index >= m_length) {
+                    throw std::out_of_range("Wrong index!");
+                }
+
+                return m_string.get()[index];
+            }
+
+            // copy c'tor
+            MyStringSP(const MyStringSP& other) {
+
+                // shallow (flat) copy
+                // m_string = other.m_string;
+                // m_length = other.m_length;
+
+                // or deep copy
+                m_length = other.m_length;
+                m_string = std::make_shared<char[]>(m_length + 1);
+                std::memcpy(m_string.get(), other.m_string.get(), m_length + 1); // copying '\0'
+            }
+
+            MyStringSP& operator=(const MyStringSP& other) {
+
+                if (this == &other) {
+                    return *this;
+                }
+
+                // shallow (flat) copy
+                // m_string = other.m_string;
+                // m_length = other.m_length;
+
+                // or deep copy
+                m_length = other.m_length;
+                m_string = std::make_shared<char[]>(m_length + 1);
+                std::memcpy(m_string.get(), other.m_string.get(), m_length + 1);  // copying '\0'
+
+                return *this;
+            }
+
+            // move c'tor
+            MyStringSP(MyStringSP&& other) noexcept {
+
+                m_string = std::move(other.m_string);
+                m_length = other.m_length;
+                other.m_length = 0;
+            }
+
+            // move assignment
+            MyStringSP& operator=(MyStringSP&& other) noexcept {
+
+                if (this == &other) {
+                    return *this;
+                }
+
+                m_string = std::move(other.m_string);
+                m_length = other.m_length;
+                other.m_length = 0;
+
+                return *this;
+            }
+        };
+
+        // =============================================================
+        // MyString implementation using std::unique_ptr
+
+        class MyStringUP
+        {
+        private:
+            std::unique_ptr<char[]> m_string;
+            std::size_t             m_length;
+
+        public:
+            // default c'tor
+            MyStringUP() : m_length{} {}
+
+            // user defined c'tor
+            MyStringUP(const char* string)
+            {
+                m_length = std::strlen(string);
+                m_string = std::make_unique<char[]>(m_length + 1);
+                std::memcpy(m_string.get(), string, m_length + 1);  // copying '\0'
+            }
+
+            // no destructor necessary
+            
+            // public interface
+            void print()
+            {
+                if (m_string.get() == nullptr) {
+                    std::println("<null>");
+                }
+                else {
+                    std::println("{}", m_string.get());
+                }
+            }
+
+            // subscript operator
+            char& operator[] (int index) {
+
+                if (index >= m_length) {
+                    throw std::out_of_range("Wrong index!");
+                }
+
+                return m_string.get()[index];
+            }
+
+            // copy c'tor
+            MyStringUP(const MyStringUP& other) {
+
+                m_length = std::strlen(other.m_string.get());
+                m_string = std::make_unique<char[]>(m_length + 1);
+                std::memcpy(m_string.get(), other.m_string.get(), m_length + 1);  // copying '\0'
+            }
+
+            // copy assignment
+            MyStringUP& operator=(const MyStringUP& other) {
+
+                if (this == &other) {
+                    return *this;
+                }
+
+                m_length = std::strlen(other.m_string.get());
+                m_string = std::make_unique<char[]>(m_length + 1);
+                std::memcpy(m_string.get(), other.m_string.get(), m_length + 1);  // copying '\0'
+
+                return *this;
+            }
+
+            // move c'tor
+            MyStringUP(MyStringUP&& other) noexcept {
+
+                m_string = std::move(other.m_string);
+                m_length = other.m_length;
+                other.m_length = 0;
+            }
+
+            // move assignment
+            MyStringUP& operator=(MyStringUP&& other) noexcept {
+
+                if (this == &other) {
+                    return *this;
+                }
+
+                m_string = std::move(other.m_string);
+                m_length = other.m_length;
+                other.m_length = 0;
+
+                return *this;
+            }
+        };
+
+        // =============================================================
+        // test routines
+
+        static void testExercise_00()
+        {
+            MyStringSP ms1{ };
+            ms1.print();
+            MyStringUP ms2{ };
+            ms2.print();
+        }
+
+        static void testExercise_01()
+        {
+            MyStringSP ms1{ "123" };
+            ms1.print();
+
+            MyStringSP ms2{ "ABCDE" };
+            ms2.print();
+
+            ms1 = ms2;   // assignment 
+            ms1.print();
+
+            ms2[0] = '?';
+
+            ms1.print();
+            ms2.print();
+        }
+
+        static void testExercise_02()
+        {
+            MyStringSP ms1{ "ABCDE" };
+            ms1.print();
+
+            MyStringSP ms2{ ms1 };  // copy construction
+            ms2.print();
+
+            ms2[0] = '?';
+
+            ms1.print();
+            ms2.print();
+        }
+
+        static void testExercise_10()
+        {
+            MyStringUP ms1{ "123" };
+            ms1.print();
+
+            MyStringUP ms2{ "ABCDE" };
+            ms2.print();
+
+            ms1 = ms2;    // assignment 
+            ms1.print();
+
+            ms2[0] = '?';
+
+            ms1.print();
+            ms2.print();
+        }
+
+        static void testExercise_11()
+        {
+            MyStringUP ms1{ "ABCDE" };
+            ms1.print();
+
+            MyStringUP ms2{ ms1 };  // copy construction
+            ms2.print();
+
+            ms2[0] = '?';
+
+            ms1.print();
+            ms2.print();
+        }
+
+        static void testExercise_20()
+        {
+            MyStringSP ms1{ "ABCDE" };
+            ms1.print();
+
+            MyStringSP ms2{ ms1 };  // copy construction
+            ms2.print();
+
+            ms1 = ms2;              // copy assignment
+            ms1.print();
+        }
+
+        static void testExercise_21()
+        {
+            MyStringSP ms1{ "ABCDE" };
+            ms1.print();
+
+            MyStringSP ms2{ std::move(ms1) };  // move construction
+            ms1.print();
+            ms2.print();
+
+            ms1 = std::move(ms2);              // move assignment
+            ms1.print();
+            ms2.print();
+        }
+
+        static void testExercise_22()
+        {
+            MyStringUP ms1{ "ABCDE" };
+            ms1.print();
+
+            MyStringUP ms2{ ms1 };             // copy construction
+            ms2.print();
+
+            ms1 = ms2;                         // copy assignment
+            ms1.print();
+        }
+
+        static void testExercise_23()
+        {
+            MyStringUP ms1{ "ABCDE" };
+            ms1.print();
+
+            MyStringUP ms2{ std::move(ms1) };  // move construction
+            ms1.print();
+            ms2.print();
+
+            ms1 = std::move(ms2);              // move assignment
+            ms1.print();
+            ms2.print();
+        }
+
+        static void testExercise() {
+
+            // default c'tors
+            testExercise_00();
+
+            // MyStringSP
+            testExercise_01();  // testing assignment
+            testExercise_02();  // testing copy construction
+
+            // MyStringUP
+            testExercise_10();  // testing assignment
+            testExercise_11();  // testing copy construction
+
+            // MyStringSP
+            testExercise_20();  // testing copy semantics
+            testExercise_21();  // testing move semantics
+
+            // MyStringUP
+            testExercise_22();  // testing copy semantics
+            testExercise_23();  // testing move semantics
         }
     }
 }
@@ -224,9 +565,11 @@ namespace Exercises_SmartPointers {
 void test_exercises_smartpointer()
 {
     using namespace Exercises_SmartPointers;
-    Exercise_01::testExercise_01();
-    // Exercise_02::testExercise_02();   // crashes when executed
-    Exercise_03::testExercise_03();
+
+    Exercise_01::testExercise();
+    // Exercise_02::testExercise();   // crashes when executed
+    Exercise_03::testExercise();
+    Exercise_04::testExercise();
 }
 
 // =====================================================================================
--- GeneralSnippets/Exercises/Exercises_15_SmartPointers.md
+++ GeneralSnippets/Exercises/Exercises_15_SmartPointers.md
@@ -15,6 +15,7 @@
 | *Aufgabe* 1 | Quiz zu Smart Pointer<br/>(Vorausetzungen: `std::shared_ptr<>`) |
 | *Aufgabe* 2 | Noch ein Quiz zu Smart Pointer<br/>(Vorausetzungen: `std::shared_ptr<>`) |
 | *Aufgabe* 3 | Betrachtungen eines &bdquo;nicht besitzenden&rdquo; Zeigers<br/>(Vorausetzungen: `std::shared_ptr<>`, `std::weak_ptr<>`) |
+| *Aufgabe* 4 | Verwendung von Smartpointern für Membervariablen einer Klasse<br/>(Vorausetzungen: `std::unique_ptr<>` und `std::shared_ptr<>`) |
 
 *Tabelle* 1: Aufgaben zu Smart Pointer.
 
@@ -192,6 +193,94 @@ für ein `std::shared_ptr`-Objekt sind.
 Das Ticket selbst steht für einen nicht-besitzenden Zeiger,
 man kann es zu einem späteren Zeitpunkt gegen ein `std::shared_ptr`-Objekt eintauschen &ndash; sofern
 das referenzierte Objekt noch existiert.
+
+---
+
+## Aufgabe 4: Verwendung von Smartpointern für Membervariablen einer Klasse
+
+#### Vorausetzungen: `std::unique_ptr<>` und `std::shared_ptr<>`
+
+In dieser Aufgabe gehen wir auf den Gebrauch eines Smartpointers als Membervariable einer Klasse ein.
+
+Schreiben Sie eine Klasse `MyString`,
+die die ersten Schritte einer Realisierung für eine Zeichenketten-Klasse darstellen könnte.
+Die Zeichen sind auf der Halde (*Heap*) abzulegen,
+ihre Anzahl soll prinzipiell beliebig groß sein dürfen.
+
+*Abbildung* 1 zeigt ein `MyString`-Objekt, das die Zeichenkette `ABCDEFGHIJ`
+der Länge 10 enthält.
+Die Variable `m_length` ist vom Typ `std::size_t`, 
+der Typ der Variablen `m_string` ist bewusst offen gehalten.
+
+Betrachten Sie genau, welche Anteile eines `MyString`-Objekts auf dem Stack
+und welche auf dem Heap liegen:
+
+<img src="cpp_exercise_my_string.svg" width="600" />
+
+*Abbildung* 1: Aufteilung eines `MyString`-Objekts auf die beiden Speicherbereiche *Stack* und *Heap*.
+
+### `std::shared_ptr`- oder `std::unique_ptr`-Objekt?
+
+Der Gebrauch von `new` und `delete` ist in einem Modern C++ Programm nicht mehr angesagt.
+Wir greifen deshalb auf eine *Smart Pointer* Klasse zurück,
+um die Adresse der Zeichen auf dem Heap zu verwalten.
+
+Es ist sorgfältig zu überlegen und zu begründen,
+ob sich hierfür ein `std::shared_ptr`- oder ein `std::unique_ptr`-Objekt besser eignet.
+
+*Hinweis*:
+Technisch gesehen ist es möglich, mit beiden Klassen eine `MyString`-Realisierung
+auf die Beine zu stellen. Bei näherem Hinsehen sollte jedoch deutlich werden,
+dass auf Grund der Anforderungen an die `MyString`-Klasse nur eine der beiden Klassen in Betracht kommt.
+
+### Minimaler Funktionsumfang der Klasse `MyString`
+
+Für eine minimale Ausstattung der `MyString`-Klasse realisieren wir
+
+  * einen benutzerdefinierten Konstruktor, der eine `const char*` Zeichenkette entgegennimmt,
+deren Zeichen im `MyString`-Objekt aufzunehmen sind,
+  * eine `print`-Methode zur Ausgabe der Zeichen in der Konsole,
+  * eine `length`-Methode, die die Anzahl der Zeichen zurückliefert und
+  * einen Index-Operator, um auf einzelne Zeichen in der Zeichenkette lesend und schreibend zugreifen zu können.
+
+
+
+| Name | Beschreibung |
+|:-|:-|
+| `MyString(const char* s)` | Benutzerdefinierter Konstruktor &ndash; die Zeichen des Parameters `s` werden in das Objekt aufgenommen. |
+| `void print()` | Ausgabe der Zeichenkette auf der Konsole. |
+| `std::size_t length()` | Liefert die Anzahl der Zeichen zurück. |
+| `char& operator[] (int index);` | Lesender oder schreibender Zugriff auf ein einzelnes Zeichen der Zeichenkette. |
+
+*Tabelle* 1: Minimaler Funktionsumfang der Klasse `MyString`.
+
+Nun kommen wir auf den zweiten Schwerpunkt der Aufgabe zu sprechen:
+
+### Regelwerk Rule-of-Zero oder Rule-of-Three?
+
+Es ist das Rule-of-Zero oder Rule-of-Three Regelwerk zu betrachten und in seiner Umsetzung
+in der Klasse `MyString` zu begründen. 
+
+1. Frage:<br />
+Kann man das Rule-of-Zero Regelwerk in Anspruch nehmen?
+ 
+In diesem Fall wäre folglich keine der drei fraglichen Methoden/Operatoren Kopierkonstruktor, Wertzuweisungsoperator und Destruktor zu realisieren.
+Verifizieren Sie, ob in Ihrer Realisierung der `MyString`-Klasse diese drei Methoden/Operatoren zur Laufzeit zur Verfügung stehen?
+Welche Ergebnisse erhalten Sie bei der Ausführung?
+
+
+2. Frage:<br />
+Wenn die Antwort auf die erste Frage 'nein' lautet,
+wie sieht dann eine Realisierung dieser drei Methoden/Operatoren aus?
+Testen Sie die Implementierung mit ensprechenden Beispielen.
+
+### Klasse `MyString` und die Verschiebe-Semantik: Regelwerk Rule-of-Five
+
+Neben dem Kopieren lassen sich ab C++ 11 Objekte auch verschieben.
+Welche Methoden/Operatoren muss die Klasse `MyString` bereitstellen,
+so dass deren Objekte auch verschoben werden können.
+
+Zeigen Sie das Verschieben an entsprechenden Beispielen auf.
 
 ---
 
